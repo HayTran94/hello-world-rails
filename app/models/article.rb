731baa52695165816ab71a58a49baad82268da43
haytv <haytv@vng.com.vn>
2024-07-02 14:22:13 +0700

    update life cycle

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,16 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+
+  after_create :log_new_user
+  after_update :log_update_user
+
+  private
+  def log_new_user
+    puts "A new user has been created #{self.title}"
+  end
+
+  def log_update_user
+    puts "A new user has been updated #{self.title}"
+  end
 end
